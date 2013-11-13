Distrotech/Mesa

i965: Update 3DSTATE_{DEPTH,STENCIL,...}_BUFFER and such for Broadwell.
(This patch was written before Paul's refactor to reuse
 brw_emit_depthbuffer and just implement a gen7_emit_depth_stencil_hiz
 function.  We should probably rearchitect it to fit that new model, as
 it's much better.  --Ken)

(The rest of this commit message was written by Chad.)

The depth buffer packet emitted by this patchis incomplete.

We must use the packet's level/layer fields to specify the miptree slice
into which to render. This patch emits 0 there, and hence all rendering
occurs to the miptree's base slice. (In gen5 through gen7, we used the
intra-tile x/y offsets to specify the miptree slice. But gen8 removed
those fields.)

However, the incomplete implementation does show some signs of life.  It
fixes the page fault in piglit:fbo-depth. (The test's segfault was
likely due to the high 16 bits of the surface base address being
invalid).

v2: [chadv]
  - Comment in patch that depth buffer packet needs level/layer
    fields.
  - Remove comments specific to gen7.
  - Expand commit message.

Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
Signed-off-by: Chad Versace <chad.versace@linux.intel.com>

src/mesa/drivers/dri/i965/gen8_depth_state.c
@@ -1,0 +1,176 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "intel_batchbuffer.h"
+#include "intel_mipmap_tree.h"
+#include "intel_regions.h"
+#include "intel_fbo.h"
+#include "brw_context.h"
+#include "brw_state.h"
+#include "brw_defines.h"
+
+static void emit_depthbuffer(struct brw_context *brw)
+{
+   struct gl_context *ctx = &brw->ctx;
+   struct gl_framebuffer *fb = ctx->DrawBuffer;
+
+   /* _NEW_BUFFERS */
+   struct intel_renderbuffer *drb = intel_get_renderbuffer(fb, BUFFER_DEPTH);
+   struct intel_renderbuffer *srb = intel_get_renderbuffer(fb, BUFFER_STENCIL);
+   struct intel_mipmap_tree *depth_mt = brw->depthstencil.depth_mt;
+   struct intel_mipmap_tree *stencil_mt = brw->depthstencil.stencil_mt;
+   struct intel_mipmap_tree *hiz_mt = NULL; /* XXX(gen8): HiZ */
+
+   assert(!stencil_mt || stencil_mt->format == MESA_FORMAT_S8);
+   assert(!depth_mt || !_mesa_is_format_packed_depth_stencil(depth_mt->format));
+
+   intel_emit_depth_stall_flushes(brw);
+
+   if (depth_mt == NULL) {
+      uint32_t dw1 = BRW_DEPTHFORMAT_D32_FLOAT << 18;
+      uint32_t dw4 = 0;
+
+      if (stencil_mt == NULL) {
+         dw1 |= BRW_SURFACE_NULL << 29;
+      } else {
+         /* _NEW_STENCIL: enable stencil buffer writes */
+         dw1 |= (ctx->Stencil._WriteEnabled) << 27;
+
+         /* 3DSTATE_STENCIL_BUFFER inherits surface type and dimensions. */
+         dw1 |= BRW_SURFACE_2D << 29;
+         dw4 = ((srb->Base.Base.Width - 1) << 4) |
+         ((srb->Base.Base.Height - 1) << 18);
+      }
+
+      BEGIN_BATCH(8);
+      OUT_BATCH(GEN7_3DSTATE_DEPTH_BUFFER << 16 | (8 - 2));
+      OUT_BATCH(dw1);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(dw4);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   } else {
+      struct intel_region *region = depth_mt->region;
+
+      /* XXX(gen8): The depth buffer packet is incomplete.
+       *
+       * We must use the packet's level/layer fields to specify the miptree
+       * slice into which to render.  (In gen5 through gen7, we used the
+       * intra-tile x/y offsets to specify the miptree slice. But gen8 removed
+       * those fields.)
+       */
+
+      /* _NEW_DEPTH, _NEW_STENCIL */
+      BEGIN_BATCH(8);
+      OUT_BATCH(GEN7_3DSTATE_DEPTH_BUFFER << 16 | (8 - 2));
+      OUT_BATCH((BRW_SURFACE_2D << 29) |
+                ((ctx->Depth.Mask != 0) << 28) |
+                ((stencil_mt != NULL && ctx->Stencil._WriteEnabled) << 27) |
+                ((hiz_mt ? 1 : 0) << 22) | /* hiz enable */
+                (brw_depthbuffer_format(brw) << 18) |
+                (region->pitch - 1));
+      OUT_RELOC(region->bo,
+                I915_GEM_DOMAIN_RENDER, I915_GEM_DOMAIN_RENDER,
+                brw->depthstencil.depth_offset);
+      OUT_BATCH(0); /* XXX(gen8): High 16-bits */
+      OUT_BATCH(((drb->Base.Base.Width - 1) << 4) |
+                ((drb->Base.Base.Height - 1) << 18));
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   }
+
+   if (hiz_mt == NULL) {
+      BEGIN_BATCH(3);
+      OUT_BATCH(GEN7_3DSTATE_HIER_DEPTH_BUFFER << 16 | (3 - 2));
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   } else {
+      BEGIN_BATCH(3);
+      OUT_BATCH(GEN7_3DSTATE_HIER_DEPTH_BUFFER << 16 | (3 - 2));
+      OUT_BATCH(hiz_mt->region->pitch - 1);
+      OUT_RELOC(hiz_mt->region->bo,
+                I915_GEM_DOMAIN_RENDER,
+                I915_GEM_DOMAIN_RENDER,
+                brw->depthstencil.hiz_offset);
+      ADVANCE_BATCH();
+   }
+
+   if (stencil_mt == NULL) {
+      BEGIN_BATCH(5);
+      OUT_BATCH(GEN7_3DSTATE_STENCIL_BUFFER << 16 | (5 - 2));
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   } else {
+      BEGIN_BATCH(5);
+      OUT_BATCH(GEN7_3DSTATE_STENCIL_BUFFER << 16 | (5 - 2));
+      /* The stencil buffer has quirky pitch requirements.  From the Graphics
+       * BSpec: vol2a.11 3D Pipeline Windower > Early Depth/Stencil Processing
+       * > Depth/Stencil Buffer State > 3DSTATE_STENCIL_BUFFER [DevIVB+],
+       * field "Surface Pitch":
+       *
+       *    The pitch must be set to 2x the value computed based on width, as
+       *    the stencil buffer is stored with two rows interleaved.
+       *
+       * (Note that it is not 100% clear whether this intended to apply to
+       * Gen7; the BSpec flags this comment as "DevILK,DevSNB" (which would
+       * imply that it doesn't), however the comment appears on a "DevIVB+"
+       * page (which would imply that it does).  Experiments with the hardware
+       * indicate that it does.
+       */
+      OUT_BATCH(HSW_STENCIL_ENABLED |
+                (2 * stencil_mt->region->pitch - 1));
+      OUT_RELOC(stencil_mt->region->bo,
+                I915_GEM_DOMAIN_RENDER, I915_GEM_DOMAIN_RENDER,
+                brw->depthstencil.stencil_offset);
+      OUT_BATCH(0); /* XXX(gen8): High 16-bits */
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   }
+
+   BEGIN_BATCH(3);
+   OUT_BATCH(GEN7_3DSTATE_CLEAR_PARAMS << 16 | (3 - 2));
+   OUT_BATCH(depth_mt ? depth_mt->depth_clear_value : 0);
+   OUT_BATCH(1);
+   ADVANCE_BATCH();
+}
+
+/**
+ * \see brw_context.state.depth_region
+ */
+const struct brw_tracked_state gen8_depthbuffer = {
+   .dirty = {
+      .mesa = _NEW_BUFFERS | _NEW_DEPTH | _NEW_STENCIL,
+      .brw = BRW_NEW_BATCH,
+      .cache = 0,
+   },
+   .emit = emit_depthbuffer,
+};

freedesktop-unofficial-mirror/mesa__mesa-test

src/mesa/drivers/dri/i965/gen8_depth_state.c
@@ -1,0 +1,157 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "intel_batchbuffer.h"
+#include "intel_mipmap_tree.h"
+#include "intel_regions.h"
+#include "intel_fbo.h"
+#include "brw_context.h"
+#include "brw_state.h"
+#include "brw_defines.h"
+
+static void emit_depthbuffer(struct brw_context *brw)
+{
+   struct gl_context *ctx = &brw->ctx;
+   struct gl_framebuffer *fb = ctx->DrawBuffer;
+
+   /* _NEW_BUFFERS */
+   struct intel_renderbuffer *drb = intel_get_renderbuffer(fb, BUFFER_DEPTH);
+   struct intel_renderbuffer *srb = intel_get_renderbuffer(fb, BUFFER_STENCIL);
+   struct intel_mipmap_tree *depth_mt = brw->depthstencil.depth_mt;
+   struct intel_mipmap_tree *stencil_mt = brw->depthstencil.stencil_mt;
+   struct intel_mipmap_tree *hiz_mt = NULL; /* XXX(gen8): HiZ */
+
+   assert(!stencil_mt || stencil_mt->format == MESA_FORMAT_S8);
+   assert(!depth_mt || !_mesa_is_format_packed_depth_stencil(depth_mt->format));
+
+   intel_emit_depth_stall_flushes(brw);
+
+   if (depth_mt == NULL) {
+      uint32_t dw1 = BRW_DEPTHFORMAT_D32_FLOAT << 18;
+      uint32_t dw4 = 0;
+
+      if (stencil_mt == NULL) {
+         dw1 |= BRW_SURFACE_NULL << 29;
+      } else {
+         /* _NEW_STENCIL: enable stencil buffer writes */
+         dw1 |= (ctx->Stencil._WriteEnabled) << 27;
+
+         /* 3DSTATE_STENCIL_BUFFER inherits surface type and dimensions. */
+         dw1 |= BRW_SURFACE_2D << 29;
+         dw4 = ((srb->Base.Base.Width - 1) << 4) |
+         ((srb->Base.Base.Height - 1) << 18);
+      }
+
+      BEGIN_BATCH(8);
+      OUT_BATCH(GEN7_3DSTATE_DEPTH_BUFFER << 16 | (8 - 2));
+      OUT_BATCH(dw1);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(dw4);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   } else {
+      struct intel_region *region = depth_mt->region;
+
+      /* XXX(gen8): The depth buffer packet is incomplete.
+       *
+       * We must use the packet's level/layer fields to specify the miptree
+       * slice into which to render.  (In gen5 through gen7, we used the
+       * intra-tile x/y offsets to specify the miptree slice. But gen8 removed
+       * those fields.)
+       */
+
+      /* _NEW_DEPTH, _NEW_STENCIL */
+      BEGIN_BATCH(8);
+      OUT_BATCH(GEN7_3DSTATE_DEPTH_BUFFER << 16 | (8 - 2));
+      OUT_BATCH((BRW_SURFACE_2D << 29) |
+                ((ctx->Depth.Mask != 0) << 28) |
+                ((stencil_mt != NULL && ctx->Stencil._WriteEnabled) << 27) |
+                ((hiz_mt ? 1 : 0) << 22) | /* hiz enable */
+                (brw_depthbuffer_format(brw) << 18) |
+                (region->pitch - 1));
+      OUT_RELOC64(region->bo,
+                  I915_GEM_DOMAIN_RENDER, I915_GEM_DOMAIN_RENDER,
+                  brw->depthstencil.depth_offset);
+      OUT_BATCH(((drb->Base.Base.Width - 1) << 4) |
+                ((drb->Base.Base.Height - 1) << 18));
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   }
+
+   if (stencil_mt == NULL) {
+      BEGIN_BATCH(5);
+      OUT_BATCH(GEN7_3DSTATE_STENCIL_BUFFER << 16 | (5 - 2));
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   } else {
+      BEGIN_BATCH(5);
+      OUT_BATCH(GEN7_3DSTATE_STENCIL_BUFFER << 16 | (5 - 2));
+      /* The stencil buffer has quirky pitch requirements.  From the Graphics
+       * BSpec: vol2a.11 3D Pipeline Windower > Early Depth/Stencil Processing
+       * > Depth/Stencil Buffer State > 3DSTATE_STENCIL_BUFFER [DevIVB+],
+       * field "Surface Pitch":
+       *
+       *    The pitch must be set to 2x the value computed based on width, as
+       *    the stencil buffer is stored with two rows interleaved.
+       *
+       * (Note that it is not 100% clear whether this intended to apply to
+       * Gen7; the BSpec flags this comment as "DevILK,DevSNB" (which would
+       * imply that it doesn't), however the comment appears on a "DevIVB+"
+       * page (which would imply that it does).  Experiments with the hardware
+       * indicate that it does.
+       */
+      OUT_BATCH(HSW_STENCIL_ENABLED |
+                (2 * stencil_mt->region->pitch - 1));
+      OUT_RELOC64(stencil_mt->region->bo,
+                  I915_GEM_DOMAIN_RENDER, I915_GEM_DOMAIN_RENDER,
+                  brw->depthstencil.stencil_offset);
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   }
+
+   BEGIN_BATCH(3);
+   OUT_BATCH(GEN7_3DSTATE_CLEAR_PARAMS << 16 | (3 - 2));
+   OUT_BATCH(depth_mt ? depth_mt->depth_clear_value : 0);
+   OUT_BATCH(1);
+   ADVANCE_BATCH();
+}
+
+/**
+ * \see brw_context.state.depth_region
+ */
+const struct brw_tracked_state gen8_depthbuffer = {
+   .dirty = {
+      .mesa = _NEW_BUFFERS | _NEW_DEPTH | _NEW_STENCIL,
+      .brw = BRW_NEW_BATCH,
+      .cache = 0,
+   },
+   .emit = emit_depthbuffer,
+};